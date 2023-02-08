32c60da8b3432d17af1030ed21adb095cfdbe493
Yves-Laurent Creton <yveslaurent.creton@gmail.com>
2023-02-08 16:15:10 +0100

    Fixed issue with mandatory script

--- src/Modules/PSJet/public/Invoke-PowerShellAsAdmin.ps1
+++ src/Modules/PSJet/public/Invoke-PowerShellAsAdmin.ps1
@@ -3,7 +3,7 @@
     This function starts a new process of PowerShell with administrator privileges.
 
     .DESCRIPTION
-    The function takes in a mandatory parameter, $Script, which is the path to the PowerShell script to be run with administrator privileges. The function sets the $scriptArgument variable based on whether the $Script parameter is provided or not. The function then starts a new process of PowerShell using the Start-Process cmdlet with the 'RunAs' verb and the argument list of "-ExecutionPolicy Bypass $scriptArgument".
+    The function takes in a parameter, Script, which is the path to the PowerShell script to be run with administrator privileges. The function sets the $scriptArgument variable based on whether the $Script parameter is provided or not. The function then starts a new process of PowerShell using the Start-Process cmdlet with the 'RunAs' verb and the argument list of "-ExecutionPolicy Bypass $scriptArgument".
 
     .PARAMETER Script
     The path to the PowerShell script to be run with administrator privileges.
@@ -19,7 +19,7 @@
 #>
 function Invoke-PowerShellAsAdmin {
     param (
-        [Parameter(Mandatory)]
+        [Parameter()]
         [string]$Script
     )
 
--- src/Modules/PSJet/public/Invoke-WindowsPowerShellAsAdmin.ps1
+++ src/Modules/PSJet/public/Invoke-WindowsPowerShellAsAdmin.ps1
@@ -3,7 +3,7 @@
     This function starts a new process of Windows PowerShell with administrator privileges.
 
     .DESCRIPTION
-    The function takes in a mandatory parameter, $Script, which is the path to the Windows PowerShell script to be run with administrator privileges. The function sets the $scriptArgument variable based on whether the $Script parameter is provided or not. The function then starts a new process of Windows PowerShell using the Start-Process cmdlet with the 'RunAs' verb and the argument list of "-ExecutionPolicy Bypass $scriptArgument".
+    The function takes in a parameter, Script, which is the path to the Windows PowerShell script to be run with administrator privileges. The function sets the $scriptArgument variable based on whether the $Script parameter is provided or not. The function then starts a new process of Windows PowerShell using the Start-Process cmdlet with the 'RunAs' verb and the argument list of "-ExecutionPolicy Bypass $scriptArgument".
 
     .PARAMETER Script
     The path to the Windows PowerShell script to be run with administrator privileges.
@@ -19,7 +19,7 @@
 #>
 function Invoke-WindowsPowerShellAsAdmin {
     param (
-        [Parameter(Mandatory)]
+        [Parameter()]
         [string]$Script
     )
 
