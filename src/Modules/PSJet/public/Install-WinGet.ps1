ab59287206c13a903535ff682e6f6ef315619066
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2024-07-09 09:14:19 +0200

    Added dependency download to Install-WinGet

--- src/Modules/PSJet/public/Install-WinGet.ps1
+++ src/Modules/PSJet/public/Install-WinGet.ps1
@@ -18,21 +18,52 @@ function Install-WinGet {
     $wingetInstalled = Test-IsWinGetInstalled
     
     if (-not $wingetInstalled) {
-        # Specify the GitHub repository details and the winget asset name
-        $owner = "Microsoft"
-        $repository = "winget-cli"
-        $asset = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
+        # Get the download folder
+        $downloadFolder = Get-DownloadsFolder
+
+        # Download and install dependencies
+        Write-Host "Downloading and installing dependencies for WinGet..."
+
+        # Determine if PowerShell version is less than 7
+        $psVersion = $PSVersionTable.PSVersion
+        $isLegacyPS = ($psVersion.Major -lt 7)
         
-        Write-Host "Downloading and installing WinGet..."
-        Install-GitHubRelease -Owner $owner -Repository $repository -Asset $asset
-        
-        # Verify installation
-        $wingetInstalled = Test-IsWinGetInstalled
-        
-        if ($wingetInstalled) {
-            Write-Host 'WinGet installed successfully.'
-        } else {
-            Write-Host 'Failed to install WinGet.'
+        if ($isLegacyPS) {
+            # Store the current value of $ProgressPreference
+            $currentProgressPreference = $ProgressPreference
+            
+            # Set $ProgressPreference to 'SilentlyContinue' to improve Invoke-WebRequest performance
+            $ProgressPreference = 'SilentlyContinue'
+        }
+
+        try {
+            # Download and install Microsoft.VCLibs.x64.14.00.Desktop.appx
+            $vclibsPath = Join-Path -Path $downloadFolder -ChildPath "Microsoft.VCLibs.x64.14.00.Desktop.appx"
+            Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $vclibsPath
+            Add-AppxPackage $vclibsPath
+
+            # Download and install Microsoft.UI.Xaml.2.8.x64.appx
+            Install-GitHubRelease -Owner "Microsoft" -Repository "microsoft-ui-xaml" -Tag "v2.8.6" -Asset "Microsoft.UI.Xaml.2.8.x64.appx"
+            
+            # Install winget
+            Write-Host "Installing WinGet..."
+
+            Install-GitHubRelease -Owner "Microsoft" -Repository "winget-cli" -Asset "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+            
+            # Verify installation
+            $wingetInstalled = Test-IsWinGetInstalled
+            
+            if ($wingetInstalled) {
+                Write-Host 'WinGet installed successfully.'
+            } else {
+                Write-Host 'Failed to install WinGet.'
+            }
+        } finally {
+            if ($isLegacyPS) {
+                # Restore the original value of $ProgressPreference
+                $ProgressPreference = $currentProgressPreference
+            }
         }
     } else {
         Write-Host 'WinGet is already installed.'
